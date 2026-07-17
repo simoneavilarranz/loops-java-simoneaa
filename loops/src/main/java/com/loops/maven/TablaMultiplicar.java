42cd0aa64d533de98155a7d0a9565394fe04d11b
unknown <avilarranz@gmail.com>
2026-07-17 14:47:23 +0200

    feat:implemented mostrarTabla method

--- loops/src/main/java/com/loops/maven/TablaMultiplicar.java
+++ loops/src/main/java/com/loops/maven/TablaMultiplicar.java
@@ -3,6 +3,15 @@ package com.loops.maven;
 public class TablaMultiplicar {
 
     public TablaMultiplicar() {
+
+    }
+
+    public void mostrarTabla(int n) {
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(n + " x " + i + " = " + (n*i));;
+        }
+
     }
     
 }
