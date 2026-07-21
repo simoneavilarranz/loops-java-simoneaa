c1d7113f063e931884acf49a9dfba7e0284f5acb
unknown <avilarranz@gmail.com>
2026-07-21 13:51:33 +0200

    feat:created tableinterface

---
+++ loops/src/main/java/com/loops/maven/MultiplicationTable.java
@@ -0,0 +1,22 @@
+package com.loops.maven;
+
+public class MultiplicationTable {
+
+    public MultiplicationTable() {
+
+    }
+
+    public void showTable(int n) {
+
+        for (int i = 1; i <= 10; i++) {
+            
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MultiplicationTable tabla = new MultiplicationTable();
+        tabla.showTable(5);
+    }
+
+}
---
+++ loops/src/main/java/com/loops/maven/TableInterface.java
@@ -0,0 +1,7 @@
+package com.loops.maven;
+
+import java.util.List;
+
+public interface TableInterface {
+    public List<String> createTable(int n);
+}
