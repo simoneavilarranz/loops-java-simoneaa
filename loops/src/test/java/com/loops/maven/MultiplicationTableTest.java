839d009dccaad71208b905e61eae61e58e23c39e
unknown <avilarranz@gmail.com>
2026-07-22 08:58:29 +0200

    feat:succesful multiplication table test

---
+++ loops/src/test/java/com/loops/maven/MultiplicationTableTest.java
@@ -0,0 +1,33 @@
+package com.loops.maven;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class MultiplicationTableTest {
+    
+    @Test
+void testMultiplicationTable() {
+    MultiplicationTable table = new MultiplicationTable();
+    
+    List<String> result = table.createTable(5);
+    
+    List<String> expected = List.of(
+        "5 x 1 = 5",
+        "5 x 2 = 10",
+        "5 x 3 = 15",
+        "5 x 4 = 20",
+        "5 x 5 = 25",
+        "5 x 6 = 30",
+        "5 x 7 = 35",
+        "5 x 8 = 40",
+        "5 x 9 = 45",
+        "5 x 10 = 50"
+    );
+    
+    assertEquals(expected, result);
+}
+
+}
