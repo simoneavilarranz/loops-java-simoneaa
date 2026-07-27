24faff5658613e3affe82cd81afbdc3014dc8133
unknown <avilarranz@gmail.com>
2026-07-27 08:48:40 +0200

    feat:new tableprinter tests

--- loops/src/test/java/com/loops/maven/TablePrinterTest.java
+++ loops/src/test/java/com/loops/maven/TablePrinterTest.java
@@ -2,6 +2,8 @@ package com.loops.maven;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -20,4 +22,22 @@ class TablePrinterTest {
         
         assertEquals(expected, result);
     }
+
+    @Test
+    void testPrintTable() {
+        TablePrinter printer = new TablePrinter();
+        List<String> input = List.of("5 x 1 = 5", "5 x 2 = 10");
+        
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        
+        printer.printTable(input);
+        
+        System.setOut(originalOut);
+        
+        String expected = "5 x 1 = 5\n5 x 2 = 10";
+        assertEquals(expected, baos.toString());
+    }
+
 }
